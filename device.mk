5c8146cde7c868ed23ae5a0702e635b7abbdd44f
Nahhk <nahhk@proton.me>
2025-04-13 19:37:34 -0500

    sapphire: Initial proprietary blobs
    Change-Id: I0e4d985ad4627d247d480425e8ecabf827c034de

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/sm6225-common/common.mk)
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sapphire/sapphire-vendor.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworksResSapphire
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
